5823b8c66974fa43d0ee224423716ced9a11db35
Gilang Raka Ramadhan <gilangrakar@gmail.com>
2024-08-31 01:30:30 +0700

    Centering table

--- resources/views/pages/data-panitia/index.blade.php
+++ resources/views/pages/data-panitia/index.blade.php
@@ -11,26 +11,26 @@
     <table id="panitia_table" class="table table-bordered" style="width:100%">
         <thead>
             <tr>
-                <th>No</th>
-                <th>Nama</th>
-                <th>No WA</th>
-                <th>Instansi</th>
-                <th>Profesi</th>
+                <th class="text-center">No</th>
+                <th class="text-center">Nama</th>
+                <th class="text-center">No WA</th>
+                <th class="text-center">Instansi</th>
+                <th class="text-center">Profesi</th>
                 @can('Reset Password')
-                    <th>Aksi</th>
+                    <th class="text-center">Aksi</th>
                 @endcan
             </tr>
         </thead>
         <tbody>
             @foreach ($data as $key => $item)
                 <tr>
-                    <td>{{ $key + 1 }}</td>
-                    <td>{{ $item->name }}</td>
-                    <td>{{ $item->ref_peserta->nomor_hp }}</td>
-                    <td>{{ $item->ref_peserta->instansi }}</td>
-                    <td>{{ $item->ref_peserta->profesi }}</td>
+                    <td class="text-center">{{ $key + 1 }}</td>
+                    <td class="text-center">{{ $item->name }}</td>
+                    <td class="text-center">{{ $item->ref_peserta->nomor_hp }}</td>
+                    <td class="text-center">{{ $item->ref_peserta->instansi }}</td>
+                    <td class="text-center">{{ $item->ref_peserta->profesi }}</td>
                     @can('Reset Password')
-                        <td>
+                        <td class="text-center">
                             <button class="btn btn-warning d-flex gap-2" data-bs-toggle="modal" data-bs-target="#resetModal"
                                 onclick="open_resetModal({{ $item->id }})"><span>Reset Password</span>
                             </button>
